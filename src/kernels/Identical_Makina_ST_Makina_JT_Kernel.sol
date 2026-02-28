ff5d1365e800c916007a2732a88ec1e4f349cca2
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2026-02-27 16:42:06 -0800

    concrete implementation of the Makina kernel

---
+++ src/kernels/Identical_Makina_ST_Makina_JT_Kernel.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import { IRoycoKernel } from "../interfaces/IRoycoKernel.sol";
+import { RoycoKernel } from "./base/RoycoKernel.sol";
+import { IdenticalMakinaSharesAdminOracleQuoter } from "./base/quoter/IdenticalMakinaSharesAdminOracleQuoter.sol";
+
+/**
+ * @title Identical_Makina_ST_Makina_JT_Kernel
+ * @author Waymont
+ * @notice The senior and junior tranches transfer in the same yield bearing Makina machine shares (DUSD, DBIT, etc.)
+ */
+contract Identical_Makina_ST_Makina_JT_Kernel is RoycoKernel, IdenticalMakinaSharesAdminOracleQuoter {
+    /**
+     * @notice Constructs the kernel state
+     * @param _params The standard construction parameters for the Royco kernel
+     * @param _makinaMachine The Makina machine for the Royco market's tranche tokens
+     */
+    constructor(
+        RoycoKernelConstructionParams memory _params,
+        address _makinaMachine
+    )
+        RoycoKernel(_params)
+        IdenticalMakinaSharesAdminOracleQuoter(_makinaMachine)
+    { }
+
+    /**
+     * @notice Initializes the Royco Kernel
+     * @param _params The standard initialization parameters for the Royco Kernel
+     * @param _initialConversionRateWAD The initial reference asset to NAV unit conversion rate, scaled to WAD precision
+     */
+    function initialize(IRoycoKernel.RoycoKernelInitParams calldata _params, uint256 _initialConversionRateWAD) external initializer {
+        // Initialize the base kernel state
+        __RoycoKernel_init(_params);
+        // Initialize the identical Makina machine shares to admin oracle quoter
+        __IdenticalMakinaSharesAdminOracleQuoter_init(_initialConversionRateWAD);
+    }
+}
--- src/kernels/base/quoter/IdenticalMakinaSharesAdminOracleQuoter.sol
+++ src/kernels/base/quoter/IdenticalMakinaSharesAdminOracleQuoter.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import { IERC20Metadata } from "../../../../lib/openzeppelin-contracts/contracts/interfaces/IERC4626.sol";
+import { IERC20Metadata } from "../../../../lib/openzeppelin-contracts/contracts/interfaces/IERC20Metadata.sol";
 import { Math } from "../../../../lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
 import { IMachine } from "../../../interfaces/external/makina/IMachine.sol";
 import { WAD, WAD_DECIMALS } from "../../../libraries/Constants.sol";
 import { IdenticalAssetsAdminOracleQuoter, IdenticalAssetsOracleQuoter } from "./base/IdenticalAssetsAdminOracleQuoter.sol";
 
 /**
- * @title IdenticalMakinaSharesOracleQuoter
+ * @title IdenticalMakinaSharesAdminOracleQuoter
  * @notice Quoter to convert tranche units (Makina machine shares) to/from NAV units by converting the shares to accounting assets and converting accounting assets to NAV units using an admin set rate
- * @dev Mandates that the base asset to NAV units uses an admin controlled oracle
+ * @dev Mandates that the accounting asset to NAV units uses an admin controlled oracle
  * @dev The senior and junior tranches must have the same Makina machine share as their tranche unit
  * @dev Use case: Convert DUSD (Tranche unit) to USDC (accounting assets) using the machine's convertToAssets and convert USDC to USD (NAV unit) using an admin set rate
  */
-abstract contract IdenticalMakinaSharesOracleQuoter is IdenticalAssetsAdminOracleQuoter {
+abstract contract IdenticalMakinaSharesAdminOracleQuoter is IdenticalAssetsAdminOracleQuoter {
     using Math for uint256;
 
     /// @dev The address of the Makina machine for the ST and JT asset
@@ -47,9 +47,17 @@ abstract contract IdenticalMakinaSharesOracleQuoter is IdenticalAssetsAdminOracl
     }
 
     /**
+     * @notice Initializes the identical Makina machine shares admin oracle quoter and the base identical assets oracle quoter
+     * @param _initialConversionRateWAD The initial conversion rate as defined by the oracle, scaled to WAD precision
+     */
+    function __IdenticalMakinaSharesAdminOracleQuoter_init(uint256 _initialConversionRateWAD) internal onlyInitializing {
+        __IdenticalAssetsAdminOracleQuoter_init(_initialConversionRateWAD);
+    }
+
+    /**
      * @notice Returns the conversion rate from tranche units to NAV units, scaled to WAD precision
-     * @dev This function assumes that the tranche token is an ERC4626 compliant vault
-     * @dev The conversion rate is calculated as the value of tranche asset in base asset * value of base asset in NAV units
+     * @dev This function assumes that the tranche token is a Makina machine's share token
+     * @dev The conversion rate is calculated as the value of tranche asset in accounting asset * value of accounting asset in NAV units
      * @return trancheToNAVUnitConversionRateWAD The conversion rate from tranche token units to NAV units, scaled to WAD precision
      */
     function getTrancheUnitToNAVUnitConversionRateWAD()
@@ -59,7 +67,7 @@ abstract contract IdenticalMakinaSharesOracleQuoter is IdenticalAssetsAdminOracl
         override(IdenticalAssetsOracleQuoter)
         returns (uint256 trancheToNAVUnitConversionRateWAD)
     {
-        // Fetch the conversion rate from the tranche asset (ERC4626 share) to its underlying asset, scaled to WAD precision
+        // Fetch the conversion rate from the tranche asset (Makina machine share) to its underlying asset, scaled to WAD precision
         uint256 trancheUnitToBaseAssetsConversionRateWAD = IMachine(MAKINA_MACHINE).convertToAssets(MACHINE_SHARES_TO_CONVERT_TO_ASSETS);
 
         // Resolve the machine's accounting asset to NAV unit conversion rate, scaled to WAD precision
